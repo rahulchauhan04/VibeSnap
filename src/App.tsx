f1ba94b902917b78f78af70dec01c2623c173793
rahulchauhan04 <rahulg0004@gmail.com>
2024-12-07 18:15:43 +0530

    firebase

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center ">
+      <h1 className="text-4xl font-extrabold text-green-600">
+        Tailwind CSS Test!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
